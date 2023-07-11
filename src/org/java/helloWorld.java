56fdce0e2fa3d969fb6081c782321c13fa2e53c4
omarebid1 <omar3bid18@gmail.com>
2023-07-11 12:37:15 +0200

    file modificato

--- src/org/java/helloWorld.java
+++ src/org/java/helloWorld.java
@@ -2,7 +2,7 @@ package org.java;
 
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.print("1 + 1 = ");
-        System.out.print(1+1);
+        System.out.print("1 + 1 != ");
+        System.out.print(1+1+3);
     }
 }
